e3ebd8def76e65b4eb2dde4eaf166bb42af53536
WuHua <wishsand@126.com>
2016-05-31 19:29:32 +0800

    shiyan7

---
+++ shiyan7.cpp
@@ -0,0 +1,15 @@
+sudo apt-get install ros-<distro>-ros-tutorials
+
+roscore
+
+rosnode list
+
+rosnode info /rosout
+
+rosrun turtlesim turtlesim_node
+
+rosnode list
+
+rosrun turtlesim turtlesim_node __name:=my_turtle 
+
+rosnode ping my_turtle
