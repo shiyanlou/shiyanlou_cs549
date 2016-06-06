73dd583805d559c60bf4df1138d08bd6b67f22be
WuHua <wishsand@126.com>
2016-06-06 15:30:46 +0800

    shiyan9

---
+++ shiyan9.cpp
@@ -0,0 +1,14 @@
+rescore
+
+rosrun turtlesim turtlesim_node
+
+rosservice list 
+
+rosservice type clear
+
+rosservice call clear 
+
+rosservice type spawn| rossrv show 
+
+rosservice call spawn 2 2 0.2 ""
+
